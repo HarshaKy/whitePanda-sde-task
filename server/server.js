c7f53bdb6b6938443a465a2171ebb8c76daff4d1
HarshaKy <harshaky6573@gmail.com>
2020-05-06 22:59:19 +0530

    getCars and add new cars from html form

--- server/server.js
+++ server/server.js
@@ -1,17 +1,45 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const _ = require('lodash')
+const path = require('path')
+const hbs = require('hbs')
 
 let {mongoose} = require('./db/mongoose')
 let {Car} = require('./models/cars')
 
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
 let app = express()
 const port = 3000
 
-app.use(bodyParser.json())
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+hbs.registerPartials(partialsPath)
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(publicDirPath))
 
 app.get('', (req, res) => {
-    res.send('hello')
+    res.render('index', {
+        title: "Index"
+    })
+})
+
+app.get('/addCars', (req, res) => {
+    res.render('addCars', {
+        title: "Add Cars",
+    })
+})
+
+app.get('/cars', (req, res) => {
+    Car.find().then((cars) => {
+        res.send(cars)
+    }, (e) => {
+        res.status(400).send(e)
+    })
 })
 
 app.post('/cars', (req, res) => {
@@ -19,7 +47,11 @@ app.post('/cars', (req, res) => {
     let car = new Car(body)
 
     car.save().then((doc) => {
-        res.send(doc)
+        res.render('addCars', {
+            title: "Add Cars",
+            text: "Added Car",
+            car: doc
+        })
     }, (e) => {
         res.status(400).send(e)
     })
